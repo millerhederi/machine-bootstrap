c1e5bbb0d57101e787e65e5de1ad0365a278c9be
Miller Hederi <millerhederi@gmail.com>
2019-02-18 14:40:00 -0500

    Add comment header to Open-Explorer.ps1

--- dotfiles/PowerShell/Scripts/Open-Explorer.ps1
+++ dotfiles/PowerShell/Scripts/Open-Explorer.ps1
@@ -1,3 +1,26 @@
+<#
+.SYNOPSIS 
+Open File Explorer to a given path.
+
+.DESCRIPTION
+Open File Explorer to a given path.
+
+.PARAMETER Path
+
+The path to open File Explorer to.
+
+.Example
+
+PS>Open-Explorer -Path "C:\dev"
+
+Opens File Explorer to the path "C:\dev".
+
+.Example
+
+PS>Open-Explorer
+
+Opens File Explorer to the current working directory.
+#>
 param (
     [Parameter(Position = 0)]
     $Path = '.'
