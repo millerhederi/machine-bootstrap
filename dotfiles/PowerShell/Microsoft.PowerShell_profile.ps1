2c48137c7cedaf2fb20262f759c018628254012e
Miller Hederi <millerhederi@gmail.com>
2019-02-20 08:42:54 -0500

    Add loading of private profiles from PowerShell profile

--- dotfiles/PowerShell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -8,4 +8,12 @@ $env:path += ";$scripts"
 Import-Module posh-git
 Import-Module z
 
+#region Load private profiles
+$PrivateProfilesRootDirectory = "$(Split-Path $Profile)\.private"
+if (Test-Path -Path $PrivateProfilesRootDirectory -PathType Container)
+{
+    Get-ChildItem -Path $PrivateProfilesRootDirectory -Include profile.ps1 -Recurse | ForEach-Object { . $_.FullName }
+}
+#endregion
+
 Set-Location -Path $dev
