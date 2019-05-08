da4c1d73441704553d72cb9c8f87ece3b21ae3ee
Miller Hederi <millerhederi.com>
2019-05-08 18:40:16 -0400

    Add -Force flag to base.ps1 commands to make idempotent

--- base.ps1
+++ base.ps1
@@ -3,8 +3,8 @@ $DotfilesDirectory = "$env:HomeDrive\dev\git\millerhederi\machine-bootstrap\dotf
 #region Powershell Profile
 $ScriptsDirectory = "$(Split-Path $Profile)\Scripts"
 New-Item -ItemType Directory -Path "$(Split-Path $Profile)" -Force
-New-Item -ItemType SymbolicLink -Path "$Profile" -Value "$DotfilesDirectory\Powershell\Microsoft.PowerShell_profile.ps1"
-New-Item -ItemType SymbolicLink -Path "$ScriptsDirectory" -Value "$DotfilesDirectory\Powershell\Scripts"
+New-Item -ItemType SymbolicLink -Path "$Profile" -Value "$DotfilesDirectory\Powershell\Microsoft.PowerShell_profile.ps1" -Force
+New-Item -ItemType SymbolicLink -Path "$ScriptsDirectory" -Value "$DotfilesDirectory\Powershell\Scripts" -Force
 $env:path += ";$ScriptsDirectory" # Add to path so we can use scripts below
 #endregion
 
@@ -27,7 +27,7 @@ Stop-Process -ProcessName explorer
 
 #region Visual Studio Code
 New-Item -ItemType Directory -Path "$env:AppData\Code\User" -Force
-New-Item -ItemType SymbolicLink -Path "$env:AppData\Code\User\settings.json" -Value "$DotfilesDirectory\VSCode\User\settings.json"
+New-Item -ItemType SymbolicLink -Path "$env:AppData\Code\User\settings.json" -Value "$DotfilesDirectory\VSCode\User\settings.json" -Force
 cinst vscode -y
 $env:Path += ";${env:ProgramFiles}\Microsoft VS Code\bin"
 code --install-extension ms-vscode.PowerShell
@@ -39,7 +39,7 @@ code --install-extension PeterJausovec.vscode-docker
 #endregion
 
 #region ConEmu
-Copy-Item -Path "$DotfilesDirectory\ConEmu\ConEmu.xml" -Destination "$env:AppData\ConEmu.xml"
+Copy-Item -Path "$DotfilesDirectory\ConEmu\ConEmu.xml" -Destination "$env:AppData\ConEmu.xml" -Force
 cinst conemu -y
 #endregion
 
@@ -54,21 +54,21 @@ New-Item -ItemType SymbolicLink -Path "$env:UserProfile\.gitconfig_plat" -Value 
 
 #region VeraCrypt
 New-Item -ItemType Directory -Path "$env:AppData\VeraCrypt" -Force
-New-Item -ItemType SymbolicLink -Path "$env:AppData\VeraCrypt\Configuration.xml" -Value "$DotfilesDirectory\VeraCrypt\Configuration.xml"
+New-Item -ItemType SymbolicLink -Path "$env:AppData\VeraCrypt\Configuration.xml" -Value "$DotfilesDirectory\VeraCrypt\Configuration.xml" -Force
 cinst veracrypt -y
 #endregion
 
 #region SublimeMerge
 New-Item -ItemType Directory -Path "$env:AppData\Sublime Merge\Packages" -Force
-New-Item -ItemType SymbolicLink -Path "$env:AppData\Sublime Merge\Packages\User" -Value "$DotfilesDirectory\SublimeMerge\Packages\User"
+New-Item -ItemType SymbolicLink -Path "$env:AppData\Sublime Merge\Packages\User" -Value "$DotfilesDirectory\SublimeMerge\Packages\User" -Force
 # There is no chocolatey package for sublime merge yet
 #endregion
 
 #region SublimeText
 New-Item -ItemType Directory -Path "$env:AppData\Sublime Text 3\Packages\User" -Force
-New-Item -ItemType SymbolicLink -Path "$env:AppData\Sublime Text 3\Packages\User\Package Control.sublime-settings" -Value "$DotfilesDirectory\SublimeText\Packages\User\Package Control.sublime-settings"
-New-Item -ItemType SymbolicLink -Path "$env:AppData\Sublime Text 3\Packages\User\Preferences.sublime-settings" -Value "$DotfilesDirectory\SublimeText\Packages\User\Preferences.sublime-settings"
-New-Item -ItemType SymbolicLink -Path "$env:AppData\Sublime Text 3\Packages\Snippets" -Value "$DotfilesDirectory\SublimeText\Packages\Snippets"
+New-Item -ItemType SymbolicLink -Path "$env:AppData\Sublime Text 3\Packages\User\Package Control.sublime-settings" -Value "$DotfilesDirectory\SublimeText\Packages\User\Package Control.sublime-settings" -Force
+New-Item -ItemType SymbolicLink -Path "$env:AppData\Sublime Text 3\Packages\User\Preferences.sublime-settings" -Value "$DotfilesDirectory\SublimeText\Packages\User\Preferences.sublime-settings" -Force
+New-Item -ItemType SymbolicLink -Path "$env:AppData\Sublime Text 3\Packages\Snippets" -Value "$DotfilesDirectory\SublimeText\Packages\Snippets" -Force
 cinst sublimetext3 -y
 Write-Output 'st3' | cinst sublimetext3.packagecontrol -y
 #endregion
