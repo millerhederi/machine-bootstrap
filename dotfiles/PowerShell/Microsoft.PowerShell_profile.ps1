5232f381eb29db56c611c5a8af733a725dfd5ff7
Miller Hederi <millerhederi.com>
2019-05-08 18:16:32 -0400

    Remove unused powershell function

--- dotfiles/PowerShell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -16,10 +16,6 @@ if (Test-Path -Path $PrivateProfilesRootDirectory -PathType Container)
 }
 #endregion
 
-function gcob ([string]$branch) {
-    git checkout -b $branch
-}
-
 function gwip ([string]$message) {
     if (!$message) {
         $message = "WIP"
