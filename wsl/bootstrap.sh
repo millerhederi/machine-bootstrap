8050f816bd8bc17aa46aadd90b963f03437ff3c4
Miller Hederi <millerhederi@gmail.com>
2019-05-11 11:12:16 -0400

    Add .tmux.conf

--- wsl/bootstrap.sh
+++ wsl/bootstrap.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+ln -sf /mnt/c/dev/git/millerhederi/machine-bootstrap/wsl/tmux/.tmux.conf ~/.tmux.conf
+
 ln -sf /mnt/c/dev/git/millerhederi/machine-bootstrap/dotfiles/Git/.gitconfig_xplat ~/.gitconfig_xplat
 ln -sf /mnt/c/dev/git/millerhederi/machine-bootstrap/dotfiles/Git/.gitconfig_linux ~/.gitconfig_plat
 cp -f /mnt/c/dev/git/millerhederi/machine-bootstrap/dotfiles/Git/.gitconfig ~/.gitconfig
