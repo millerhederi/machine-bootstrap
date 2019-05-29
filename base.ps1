73441e4041c622db997b4cfd1f8a714e7953b45f
Miller Hederi <millerhederi@gmail.com>
2019-05-29 08:41:32 -0400

    Add auto self-elevation to base.ps1

--- base.ps1
+++ base.ps1
@@ -1,3 +1,11 @@
+# Check if running as an adminstrator, if not, self elevate
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+    # Self elivate. Must set execution policy at least to RemoteSigned for the chocolately installs
+    Start-Process powershell -Verb runAs -ArgumentList "-NoExit -ExecutionPolicy RemoteSigned -Command `"& $($MyInvocation.MyCommand.Definition)`""
+    return
+}
+
 $DotfilesDirectory = "$env:HomeDrive\dev\git\millerhederi\machine-bootstrap\dotfiles"
 
 #region Powershell Profile
