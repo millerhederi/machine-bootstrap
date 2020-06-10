3956694f89e1e61b85d44d809d2779cca937b330
Miller Hederi <millerhederi@gmail.com>
2020-06-10 16:58:05 -0400

    Update installed PS modules to include AWS.Tools

--- dotfiles/PowerShell/InstallModules.ps1
+++ dotfiles/PowerShell/InstallModules.ps1
@@ -2,3 +2,6 @@ Install-Module -Name posh-git -Scope CurrentUser -Force
 Install-Module -Name oh-my-posh -Scope CurrentUser -Force
 Install-Module -Name z -Scope CurrentUser -Force
 Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+
+Install-Module -Name AWS.Tools.Installer -Force
+Install-AWSToolsModule AWS.Tools.ECS,AWS.Tools.ECR -CleanUp -Confirm:$False
