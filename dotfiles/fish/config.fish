10bf01997b8154ea0103f6eabf42a88d21972182
Miller Hederi <mhederi@fastmail.com>
2024-03-11 14:02:44 -0700

    Add fish alias for git-prune

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -5,3 +5,9 @@ end
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 set -gx PATH $PATH ~/go/bin
+
+function git-prune -d "Delete all local git branches except for main"
+    git branch | grep -v "main" | xargs git branch -D
+end
+
+funcsave git-prune
