e844ebd925f4280246a148a1c1ea694d01e74a17
Miller Hederi <mhederi@fastmail.com>
2024-03-14 16:14:09 -0700

    Setup starship in powershell

--- dotfiles/powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -5,3 +5,5 @@ if (Test-Path -Path $PrivateProfilesRootDirectory -PathType Container)
     Get-ChildItem -Path $PrivateProfilesRootDirectory -Include profile.ps1 -Recurse | ForEach-Object { . $_.FullName }
 }
 #endregion
+
+Invoke-Expression (&starship init powershell)
