28e1b4c86ed150b972197f446e81d676046d05bf
Miller Hederi <mhederi@fastmail.com>
2024-03-14 16:05:55 -0700

    Add dotfiles for powershell

---
+++ dotfiles/powershell/.config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+#region Load private profiles
+$PrivateProfilesRootDirectory = "$(Split-Path $Profile)\.private"
+if (Test-Path -Path $PrivateProfilesRootDirectory -PathType Container)
+{
+    Get-ChildItem -Path $PrivateProfilesRootDirectory -Include profile.ps1 -Recurse | ForEach-Object { . $_.FullName }
+}
+#endregion
